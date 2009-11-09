a579646f631efd1df74d4456a99d668796791f21
cprobert <cprobert@8b347208-cc55-11de-b058-13285e7dd63e>
2009-11-09 16:04:04 +0000

    Added call back on player stop so there's less a gap on changeover Now remembers users volume preferences All links now open in a new 'browser' window

--- scripts/prefs.js
+++ scripts/prefs.js
@@ -6,18 +6,33 @@ var prefs = {
 	weburl: "",
 	streamurl: "",
 	streamport: 80,
-	startingvol: .8,
 	buffersize: 1024,
 	twitterusername: "",
 	header: "",
+	debug: false,
 	init: function(){
+		this.debug = true;
 		this.stationname = "DapperFM";
 		this.weburl = "http://www.dapperfm.co.uk";
 		this.streamurl = "http://93.89.82.244";
 		this.streamport = 8096;
-		this.startingvol = .8;
-		this.buffersize = 1024;
+		this.buffersize = 800;
 		this.twitterusername = "dapperfm";
 		this.header = "<h1>Dapper <em style='color:#5E7E19;'>FM</em></h1><h2>&nbsp; &nbsp; &nbsp; our community <br /> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; your radio station</h2>";
+	},
+	startingvol: function(volume){
+		if(volume){ //Set
+			var data = new air.ByteArray(); // Peristant save the volume to the users 
+			data.writeUTFBytes(volume);
+			air.EncryptedLocalStore.setItem('volume', data);
+			return volume;
+		}
+		else { //Get
+			volume = air.EncryptedLocalStore.getItem('volume');
+			if (volume) 
+				return volume.readUTFBytes(volume.bytesAvailable);
+			else 
+				return 0.8;
+		}
 	}
 }
