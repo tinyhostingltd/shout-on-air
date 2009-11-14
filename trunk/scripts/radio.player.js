2cd146ff153bfde527cff69487c3256b762b248c
cprobert <cprobert@8b347208-cc55-11de-b058-13285e7dd63e>
2009-11-14 15:28:15 +0000

    All bugs I know of have been fixed and I've rearranged the folder structure

--- trunk/scripts/radio.player.js
+++ trunk/scripts/radio.player.js
@@ -1,43 +1,64 @@
 /**
  * @author Courtenay Probert
  */
-//Global vars used in the call back events (because this is nonger the radion object)
+//Global vars used in the call back events
 var bufferloaded = false;
-buffersize = 500;
-
+var buffersize = 500;
+var buffertime = 2;
+function loaderror(msg){
+	alert("The station could not be loaded: " + msg);
+}
 var radio = {
 	playing: false,
 	s: null,
+	uri: "",
 	req: null,
 	context: null,
 	channel: null,
 	pausePosition: 0,
 	buffertime: 2,
 	debug: true,
-	init: function(mp3){
-        this.log("You instantiated a Class! "+ mp3);
-		this.req = new air.URLRequest(mp3); 
-		//this.req.cacheResponse = false;	
+	init: function(uri){
+		this.uri = uri;
+        this.log("You instantiated: "+ this.name);
     },
 	load: function(){
 		bufferloaded = false;
+		var rnduri = this.uri + "/;" + Math.round(Math.random() * 100000) + ".mp3"
+		this.req = new air.URLRequest(rnduri); 
+		this.req.cacheResponse = false;	
+		this.req.useCache = false;
+		this.log("Loading: "+ rnduri);
+		
 		this.s = new air.Sound();
-		this.s.addEventListener(air.IOErrorEvent.IO_ERROR, this.error); 
-		this.s.addEventListener(air.ProgressEvent.PROGRESS, this.progress); 
-		this.context = new air.SoundLoaderContext(buffersize, true); //size of the buffer 1024
+		this.s.addEventListener(air.IOErrorEvent.IO_ERROR, this.e.error); 
+		this.s.addEventListener(air.ProgressEvent.PROGRESS, this.e.progress); 
+		this.context = new air.SoundLoaderContext(buffersize, true); 
 		this.s.load(this.req, this.context); 
+		this.log("Loaded player: "+ this.name);
 	},
 	unload: function(){
-		this.s.close();
-		this.context = null;
-		//this.req = null;
-		this.s = null;
+		if (this.s != null) {
+			try {
+				this.s.close();
+			}
+			catch(err){
+				this.log("s.close - err: "+ err);
+			}
+			this.context = null;
+			this.req = null; //I've moved initialisation from load to the constructor, so dont close here
+			this.s = null;
+			this.log("Unloaded player: "+ this.name);
+		}
 	},
 	play: function(callback){
 		if (bufferloaded) {
 			this.log("Buffer is loaded - Now playing");
 			this.playing = true;
 			this.channel = this.s.play(); //this.pausePosition
+			if(this.buffertime > 1)
+				this.buffertime -= 1;
+			this.log("Started player: "+ this.name);
 			
 			if(typeof callback == 'function'){
 		        callback.call(this); // Stop other player
@@ -45,37 +66,44 @@ var radio = {
 		}
 		else{
 			$(this).log("Buffer not loaded - trying again in "+ this.buffertime +" seconds");
-			$(this).oneTime(this.buffertime *1000, function() {
+			$(this).oneTime(this.buffertime *1000, function(event) {
 				this.buffertime += 1;
 				this.play(callback);
 			});
 		}
 	},
 	stop: function(){
-		this.playing = false;
-		try {
-			this.pausePosition = this.channel.position; //not using at the mo
-			this.channel.stop();
-		} 
-		catch (err) {
-			air.trace("Error on stop: "+ err);
+		
+		if (this.channel == null) {
+			this.playing = false;
 		}
-	  	this.unload(); // this should clean the memory
+		else {
+			try {
+				this.pausePosition = this.channel.position; //not using at the mo
+				this.channel.stop();
+				this.playing = false;
+				this.log("Stopped player: "+ this.name);
+			} 
+			catch (err) {
+				this.log("Error on stopplayer ("+ this.name +") "+ err);
+			}
+			this.unload(); // this should clean the memory
+		}
 	},
-	progress: function(event){
-		// Could i flush when event.bytesLoaded  = x ??? (stop and close)
-		var loadedPct = Math.round(100 * (event.bytesLoaded / buffersize)); 
-    	//air.trace("Buffer: " + event.bytesLoaded + " - buffersize:" + buffersize);
-		if (loadedPct >= 100)
-			bufferloaded = true;
-		else
-			$(this).log("Buffer: "+ loadedPct +"%");
-	},
-	error: function(event){
-		alert("The station could not be loaded: " + event.text); 
-		this.stop();
-		$("#stop").hide(); //these shoudnt realy be in here 
-		$("#play").show();
+	e: { // 'this' = 'window' on these event callbacks
+		progress: function(event){
+			// Could i flush when event.bytesLoaded  = x ??? (stop and close)
+			var loadedPct = Math.round(100 * (event.bytesLoaded / buffersize));
+			//air.trace("Buffer: " + event.bytesLoaded + " - buffersize:" + buffersize);
+			if (loadedPct >= 100) 
+				bufferloaded = true;
+			else 
+				$(this).log("Buffer: " + loadedPct + "%");
+		},
+		error: function(event){
+			loaderror(event.text);
+			//this.stop(); // I believe 'this' = window here because the calling object is 'window.runtime.flash.events.IOErrorEvent'
+		}
 	},
 	log: function(msg){
 		if(this.debug)
@@ -89,88 +117,85 @@ var shoutcastradio = {
 	player1: {},
 	player2: {},
 	debug: false,
+	toggletime: 3600,
+	i: 0,
 	init: function(streamurl, streamport){
 		this.log("Loading Shoutcast Radio");
-		if (streamurl != "")
+		if (streamurl != "") 
 			this.url = streamurl;
-			
-		if (streamport != "")
+		
+		if (streamport != "") 
 			this.port = streamport;
 		
 		if (this.url && this.port) {
-			
-			var streamRndURL = this.url + ":" + this.port + "/;" + Math.round(Math.random() * 100000) + ".mp3"; //every instance is on a seperate stream
-			this.player1 = $.extend(true,{},radio);
+			var streamRndURL = this.url + ":" + this.port; //every instance is on a seperate stream
+			this.player1 = $.extend(true, {name: 'player1'}, radio);
 			this.player1.debug = this.debug;
 			this.player1.init(streamRndURL);
 			
-			streamRndURL = this.url + ":" + this.port + "/;" + Math.round(Math.random() * 100000) + ".mp3";
-			this.player2 = $.extend(true,{},radio);
+			streamRndURL = this.url + ":" + this.port;
+			this.player2 = $.extend(true, {name: 'player2'}, radio);
 			this.player2.debug = this.debug;
 			this.player2.init(streamRndURL);
-			
-			//this.play();
 		}
 		else {
 			alert("No URL or Port!");
-			this.log("url: "+ this.url);
-			this.log("port: "+ this.port);
+			this.log("url: " + this.url);
+			this.log("port: " + this.port);
 		}
-	},
-	toggle: function(){
-		// This toggles the players to clean the memory perodicaly (1 hours [3600 secs])
-		$(document).everyTime((3600 * 1000), "radioSwap", function(i){
-		if (i == 3) {
-			$(this).log("Refreshing window...");
-			window.location.reload(true); //Theres an air problem with loading too many sound objects
-		}
-			
-		$(this).log("Swaping players.  Iteration: "+ i);
-		
-		if (this.player1.playing) {
-			this.player2.load()//.log("loading player 2");
-			this.player2.play(function(){
-				//this.player1.channel = new air.SoundTransform(0.1, 0);
-				air.trace("Stoping player 1");
-				this.player1.stop();
-			})//.log("playing player 2");
-		}
-		else{
-			this.player1.load()//.log("loading player 1");
-			this.player1.play(function(){
-				//this.player2.channel = new air.SoundTransform(0.1, 0);
-				//air.trace("Stoping player 2");
-				this.player2.stop();	
-			})//.log("playing player 1");
-		}
-	});		
 	},
 	play: function(callback){
-			this.player1.load();
-			//playSound("/sound/jingle.mp3"); // Should add intro jingle here
-			this.player1.play(function(){
-				if (typeof callback == 'function') 
-					callback.call(this);
-			});
+			$(this).log("Swaping players.  Iteration: " + shoutcastradio.i);
 			
-			this.toggle();		
+			if (shoutcastradio.player1.playing) {
+				shoutcastradio.player2.load()
+				shoutcastradio.player2.play(function(){
+					/*
+					for (i = 1; i <= 0; i-0.1) {
+						shoutcastradio.player1.channel.soundTransform = new air.SoundTransform(i, 0);
+					}
+					*/
+					shoutcastradio.player1.stop();
+					if (typeof callback == 'function') 
+						callback.call(this);
+				});
+			}
+			else {
+				shoutcastradio.player1.load()//.log("loading player 1");
+				shoutcastradio.player1.play(function(){
+					/*
+					for (i = 1; i <= 0; i - 0.1) {
+						shoutcastradio.player2.channel.soundTransform = new air.SoundTransform(i, 0);
+						$(this).log(i);
+					}
+					*/
+					shoutcastradio.player2.stop();
+					if (typeof callback == 'function') 
+						callback.call(this);
+				});
+			}
+			shoutcastradio.i ++;
+			setTimeout(shoutcastradio.play, shoutcastradio.toggletime * 1000); // This toggles the players to clean the memory perodicaly (1 hours = 3600 secs)
 	},
 	stop: function(callback){
-		if (this.player1.playing)
+		if (this.player1.playing) 
 			this.player1.stop();
 		
-		if (this.player2.playing)
+		if (this.player2.playing) 
 			this.player2.stop();
 		
-		if(typeof callback == 'function')
-			callback.call(this); 
+		if (typeof callback == 'function') 
+			callback.call(this);
 	},
 	restart: function(callback){
 		this.stop();
-		this.play(callback);
+		this.play(function(){
+			if (typeof callback == 'function') 
+				callback.call(this);
+		});
 	},
 	log: function(msg){
-		if(this.debug)
-      		air.trace(msg);
+		if (this.debug) 
+			air.trace(msg);
 	}
 }
